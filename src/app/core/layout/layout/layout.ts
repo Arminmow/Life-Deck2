65893e80a4c8aac0fb27f38ce4087739b67b6b2b
Armin <arminmow@gmail.com>
2025-07-20 17:19:51 +0330

    Add Google sign-in button to layout
    Introduces a temporary sign-in button to the layout component for testing purposes. The button triggers Google authentication via SupabaseService and will be moved to the login page in the future.

--- src/app/core/layout/layout/layout.ts
+++ src/app/core/layout/layout/layout.ts
@@ -6,28 +6,42 @@ import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { NzButtonModule } from 'ng-zorro-antd/button';
-import { SidebarMenu } from "../sidebar-menu/sidebar-menu";
+import { SidebarMenu } from '../sidebar-menu/sidebar-menu';
+import { SupabaseService } from '../../../supabase/supabase.service';
 
 @Component({
   selector: 'app-layout',
-  imports: [NzLayoutModule, NzMenuModule, NzIconModule, CommonModule, NzDrawerModule, NzButtonModule, SidebarMenu],
+  imports: [
+    NzLayoutModule,
+    NzMenuModule,
+    NzIconModule,
+    CommonModule,
+    NzDrawerModule,
+    NzButtonModule,
+    SidebarMenu,
+  ],
   templateUrl: './layout.html',
-  styleUrl: './layout.scss'
+  styleUrl: './layout.scss',
 })
 export class LayoutComponent implements OnInit {
   isMobile = false;
   isSidebarCollapsed = false;
   isDrawerVisible = false;
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private supabaseService: SupabaseService
+  ) {}
 
   ngOnInit(): void {
-    this.breakpointObserver.observe([Breakpoints.Handset]).subscribe(result => {
-      this.isMobile = result.matches;
-      if (!this.isMobile) {
-        this.isDrawerVisible = false;
-      }
-    });
+    this.breakpointObserver
+      .observe([Breakpoints.Handset])
+      .subscribe((result) => {
+        this.isMobile = result.matches;
+        if (!this.isMobile) {
+          this.isDrawerVisible = false;
+        }
+      });
   }
 
   toggleSidebar() {
@@ -36,5 +50,9 @@ export class LayoutComponent implements OnInit {
     } else {
       this.isSidebarCollapsed = !this.isSidebarCollapsed;
     }
+  }
+  // this is test will move to login page later
+  async signIn() {
+    this.supabaseService.signInWithGoogle();
   }
 }
