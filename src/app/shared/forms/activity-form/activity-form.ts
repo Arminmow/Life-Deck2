4885e8f737ccd3c1618efbe885979fea2a131016
Armin <arminmow@gmail.com>
2025-07-21 17:16:18 +0330

    Added activity form for adding a new activity

--- src/app/shared/forms/activity-form/activity-form.ts
+++ src/app/shared/forms/activity-form/activity-form.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import {
+  FormGroup,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-activity-form',
-  imports: [],
+  imports: [NzFormModule, ReactiveFormsModule, NzInputModule, CommonModule],
   templateUrl: './activity-form.html',
-  styleUrl: './activity-form.scss'
+  styleUrl: './activity-form.scss',
 })
-export class ActivityForm {
+export class ActivityForm implements OnInit {
+  constructor(private fb: NonNullableFormBuilder) {}
 
+  form!: FormGroup;
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(3)]],
+      description: [''],
+      icon: [''],
+      banner: [''],
+      lastActive: [''],
+    });
+  }
+
+  get titleControl() {
+    return this.form.get('title');
+  }
 }
--- src/app/shared/modals/activity-modal/activity-modal.ts
+++ src/app/shared/modals/activity-modal/activity-modal.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzModalModule } from 'ng-zorro-antd/modal';
+import { ActivityForm } from "../../forms/activity-form/activity-form";
 
 @Component({
   selector: 'app-activity-modal',
-  imports: [NzModalModule, NzButtonModule],
+  imports: [NzModalModule, NzButtonModule, ActivityForm],
   templateUrl: './activity-modal.html',
   styleUrls: ['./activity-modal.scss'],
 })
