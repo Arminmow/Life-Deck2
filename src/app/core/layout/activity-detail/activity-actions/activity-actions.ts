9c68569c3685e0fa793973ac97159625fcca74e2
Armin <arminmow@gmail.com>
2025-07-24 12:12:50 +0330

    implemented delete activity functionality

--- src/app/core/layout/activity-detail/activity-actions/activity-actions.ts
+++ src/app/core/layout/activity-detail/activity-actions/activity-actions.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { Activity, ActivityStore } from '../../../stores/activity.store';
 
 @Component({
   selector: 'app-activity-actions',
   imports: [NzIconModule, NzButtonModule],
   templateUrl: './activity-actions.html',
   styleUrl: './activity-actions.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ActivityActions {}
+export class ActivityActions {
+  constructor(private activityStore: ActivityStore) {}
+
+  @Input() activity!: Activity;
+
+  delete() {
+    this.activityStore.removeActivityEffect(this.activity.id);
+  }
+}
