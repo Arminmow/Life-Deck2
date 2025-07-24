6ccd90613b2aee30369f6dae2b653a5ba23e5709
Armin <arminmow@gmail.com>
2025-07-24 16:48:47 +0330

    minor tweaks

--- src/app/core/layout/activity-detail/activity-actions/activity-actions.ts
+++ src/app/core/layout/activity-detail/activity-actions/activity-actions.ts
@@ -2,18 +2,31 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { Activity, ActivityStore } from '../../../stores/activity.store';
+import { NzModalModule, NzModalService } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'app-activity-actions',
-  imports: [NzIconModule, NzButtonModule],
+  imports: [NzIconModule, NzButtonModule, NzModalModule],
   templateUrl: './activity-actions.html',
   styleUrl: './activity-actions.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ActivityActions {
-  constructor(private activityStore: ActivityStore) {}
+  constructor(private activityStore: ActivityStore , private modal: NzModalService) {}
 
   @Input() activity!: Activity;
+
+    confirmDelete(): void {
+    this.modal.confirm({
+      nzTitle: 'Are you sure you want to delete this?',
+      nzContent: 'This action cannot be undone.',
+      nzOkText: 'Yes, delete it',
+      nzOkType: 'primary',
+       nzOkDanger: true, 
+      nzOnOk: () => this.delete(),
+      nzCancelText: 'Cancel'
+    });
+  }
 
   delete() {
     this.activityStore.removeActivityEffect(this.activity.id);
