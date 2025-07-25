ec41e97f1c9ede3dc87ff8e6d350685040fa4a40
Armin <arminmow@gmail.com>
2025-07-25 13:45:57 +0330

    Implemented guards for /login and /dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './core/layout/layout/layout';
 import { LoginLayout } from './core/layout/login-layout/login-layout';
+import { NoAuthGuard } from './core/guards/no-auth-guard';
+import { AuthGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
-  { path: 'dashboard', component: LayoutComponent },
-  { path: 'login', component: LoginLayout },
+  { path: 'dashboard', component: LayoutComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginLayout, canActivate: [NoAuthGuard] },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
