f87e38b1722491cedead81950f5a302fdfa8ba32
Armin <arminmow@gmail.com>
2025-08-20 12:13:13 +0330

    timeAgo pipe

--- src/app/components/activity/activity-stats/activity-stats.ts
+++ src/app/components/activity/activity-stats/activity-stats.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { ActivityBtn } from '../activity-btn/activity-btn';
 import { TimeSpent } from '../../../pipes/time-spent/time-spent-pipe';
 import { Activity } from '../../../models/activity.model';
+import { TimeAgoPipe } from '../../../pipes/timeAgo/time-ago-pipe';
 
 @Component({
   selector: 'app-activity-stats',
-  imports: [NzIconModule, ActivityBtn, CommonModule, TimeSpent],
+  imports: [NzIconModule, ActivityBtn, CommonModule, TimeSpent , TimeAgoPipe],
   templateUrl: './activity-stats.html',
   styleUrl: './activity-stats.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
