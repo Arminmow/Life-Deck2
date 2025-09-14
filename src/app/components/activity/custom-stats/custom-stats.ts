72d0185765b21e28be674f1c7d38b9406a1604a2
Armin <arminmow@gmail.com>
2025-09-14 18:26:15 +0330

    add logic

--- src/app/components/activity/custom-stats/custom-stats.ts
+++ src/app/components/activity/custom-stats/custom-stats.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { ActivityStore } from '../../../stores/activity/activity.store';
 import { CommonModule } from '@angular/common';
@@ -6,6 +6,14 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzPopoverModule } from 'ng-zorro-antd/popover';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ActivityEffects } from '../../../stores/activity/activity.effects';
+import { Activity } from '../../../models/activity.model';
 
 @Component({
   selector: 'app-custom-stats',
@@ -16,20 +24,56 @@ import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
     NzPopoverModule,
     NzFormModule,
     NzInputNumberModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './custom-stats.html',
   styleUrl: './custom-stats.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CustomStats {
-  constructor(public activityStore: ActivityStore) {
+export class CustomStats implements OnInit {
+  constructor(
+    public activityStore: ActivityStore,
+    private fb: FormBuilder,
+    private activityEffectService: ActivityEffects
+  ) {
     activityStore.selectedActivity$.subscribe((ac) => {
       console.log(ac?.customStats);
     });
   }
 
+  form!: FormGroup;
+  activity!: Activity | null;
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      statName: ['', Validators.required],
+      initialValue: [''],
+    });
+
+    this.activityStore.selectedActivity$.subscribe((ac) => {
+      this.activity = ac;
+    });
+  }
+
   visible: boolean = false;
 
-  clickMe(): void {
+  clickMe(e: MouseEvent): void {
+    e.preventDefault();
+
+    if (this.form.valid && this.activity) {
+      const updatedActivity: Activity = {
+        ...this.activity, // now TS knows activity is defined
+        id: this.activity.id, // ensure id is included
+        customStats: {
+          ...this.activity.customStats,
+          [this.form.value.statName]: this.form.value.initialValue,
+        },
+      };
+
+      this.activityEffectService.updateActivityEffect(updatedActivity);
+    }
+
     this.visible = false;
+    console.log(this.form.value);
   }
 }
