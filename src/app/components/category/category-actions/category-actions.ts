28735047b9799af5571b927768354962ec7d2845
Armin <arminmow@gmail.com>
2025-10-05 22:08:29 +0330

    minor fixes

--- src/app/components/category/category-actions/category-actions.ts
+++ src/app/components/category/category-actions/category-actions.ts
@@ -6,7 +6,6 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AddToCategoryModal } from '../../../shared/modals/add-to-category-modal/add-to-category-modal';
 import { EditCategoryModal } from '../../../shared/modals/edit-category-modal/edit-category-modal';
-import { ActivityStore } from '../../../stores/activity/activity.store';
 import { Category } from '../../../models/category.model';
 import { CategoryStore } from '../../../stores/category/category.store';
 
